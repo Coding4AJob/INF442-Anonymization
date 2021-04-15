3284428d2b8f55ef7b558a22e217db6803eb98d2
LucienJi <984592975@qq.com>
2021-04-15 18:30:09 +0200

    Update_LogisticRegression
    Update

---
+++ INF442_Project/build/CMakeFiles/INF442_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/INF442_Project.dir/kernel.cpp.o"
+  "CMakeFiles/INF442_Project.dir/main.cpp.o"
+  "CMakeFiles/INF442_Project.dir/svm.cpp.o"
+  "INF442_Project"
+  "INF442_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/INF442_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ INF442_Project/build/CMakeFiles/INF442_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for INF442_Project.
+# This may be replaced when dependencies are built.
